f3496a224ce5f7954811f81b52e364480858f68a
SergeyReznikov <tikto01@mail.ru>
2021-04-08 20:49:50 +0400

    Views added

--- TravelAgency/TravelAgencyView/WindowMain.xaml.cs
+++ TravelAgency/TravelAgencyView/WindowMain.xaml.cs
@@ -5,9 +5,9 @@ namespace TravelAgencyView
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WindowMain : Window
     {
-        public MainWindow()
+        public WindowMain()
         {
             InitializeComponent();
         }
---
+++ TravelAgency/TravelAgencyView/WindowTrips.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TravelAgencyView
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowTrips.xaml
+    /// </summary>
+    public partial class WindowTrips : Window
+    {
+        public WindowTrips()
+        {
+            InitializeComponent();
+        }
+    }
+}
